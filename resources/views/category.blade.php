0fd57da1461c77bc4a96d368b11c9deb14d30dcc
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-12-03 04:03:22 +0700

    confirm fail -*- try again tmr// 12 hours left!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -48,9 +48,9 @@
             <td>{{$i++}}</td>
             <td>{{$d->Name}}</td>
             <td>
-              <button class="btn btn-info">Edit</button>&nbsp;
-            <button class="btn btn-danger" onclick="cnfrm()" name="id" value="{{$d->CatId}}" form="delCat">Delete</button>
-          </td>
+              <button type="button" class="btn btn-info" data-toggle="modal" data-target="#EditCat">Edit</button>&nbsp;
+              <button class="btn btn-danger" name="id" value="{{$d->CatId}}" form="delCat">Delete</button>
+            </td>
           </tr>
           @endforeach
         </tbody>
@@ -82,14 +82,35 @@
           </div>
         </div>
       </div>
-    </div>
-    <script type="text/javascript">
-      function cnfrm() {
-        confirm("Do you really want to delete this category?");
-      }
-    </script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  </body>
-  </html>
+
+      <!-- Modal -->
+      <div class="modal fade" id="EditCat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">New Category</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form action="<?php echo url('/'); ?>/createCat">
+              <div class="modal-body">
+
+                Category Name: 
+                <input type="text" name="catName" class="form-control">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Confirm</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    </body>
+    </html>
