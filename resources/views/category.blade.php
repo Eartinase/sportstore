6d56ebcb78e75924313be5dc233712aae7971c4f
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-12-03 03:43:09 +0700

    confirm before deleting category

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -48,7 +48,7 @@
             <td>{{$i++}}</td>
             <td>{{$d->Name}}</td>
             <td><button class="btn btn-info">Edit</button>&nbsp;
-            <button class="btn btn-danger" name="id" value="{{$d->CatId}}" form="delCat">Delete</button></td>
+            <button class="btn btn-danger" onclick="cnfrm()" name="id" value="{{$d->CatId}}" form="delCat">Delete</button></td>
           </tr>
           @endforeach
         </tbody>
@@ -81,7 +81,11 @@
         </div>
       </div>
     </div>
-
+    <script type="text/javascript">
+      function cnfrm() {
+        confirm("Do you really want to delete this category?");
+      }
+    </script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
