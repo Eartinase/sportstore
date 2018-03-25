1298ad709daf12f7e792a652093c2f3e73d0ae6d
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2018-03-25 19:21:17 +0700

    try jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,9 @@
     .bold{
         font-weight: bold;
     }    
+     .bgcolor{
+        background-color: red; 
+    }    
 </style>
 </head>
 <body>
@@ -31,7 +34,8 @@
     <?php $i = 0 ?>
     @foreach ($data as $d)
     @if($i < 4)
-    <div class="card col-md-3">
+    <div class="card col-md-3 select">
+        <br>
         <img class="card-img-top" src="{{ $d->Image }}" alt="Card image cap">
         <div class="card-body">
             <h4 class="card-title"> {{ $d->Name }} </h4>
@@ -48,7 +52,8 @@
 <br>
 <div class="row">
     <?php $i = 1 ?>
-    <div class="card col-md-3" >
+    <div class="card col-md-3 select" >
+        <br>
         <img class="card-img-top" src="{{ $d->Image }}" alt="Card image cap">
         <div class="card-body">
             <h4 class="card-title"> {{ $d->Name }}</h4>
@@ -68,8 +73,16 @@
 </div>
 
 </div>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+    $(".select").click(function(){
+        $(this).toggleClass("bgcolor");
+    });
+});
+</script>
 </body>
 </html>
